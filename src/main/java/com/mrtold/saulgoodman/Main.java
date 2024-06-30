b2749d15b3211da723047a15f81da4b2666acb63
Miron Barykin <barykinmiron@gmail.com>
2024-06-30 09:49:32 +0200

    authentication fix. another attempt

--- src/main/java/com/mrtold/saulgoodman/Main.java
+++ src/main/java/com/mrtold/saulgoodman/Main.java
@@ -178,6 +178,7 @@ public class Main {
 
     public static void main(String[] args) {
         Logger log = LoggerFactory.getLogger(Main.class);
+
         try {
             new Main();
         } catch (Exception e) {
